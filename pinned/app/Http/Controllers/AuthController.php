f15477a28d5569ab14562bf01cbdbfbb686e4e62
lsallop <lsallop3012@g.educaand.es>
2026-01-28 20:57:38 +0100

    Creacion de controladores

--- pinned/app/Http/Controllers/AuthController.php
+++ pinned/app/Http/Controllers/AuthController.php
@@ -1,10 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // Muestra la vista con el formulario de login
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    // Procesa el login del usuario
+    public function login(Request $request)
+    {
+        $credenciales = $request->only('email', 'password');
+
+        // Busca el usuario y lo autentica
+        if (Auth::attempt($credenciales)) {
+            // Regenerar la sesión para evitar fijación de sesión
+            $request->session()->regenerate();
+            return redirect()->route('dashboard.index');
+        } else {
+            return back()->withErrors([
+                'email' => 'Inicio de sesión fallido',
+            ]);
+        }
+    }
+
+    // Procesa el logout del usuario
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('home.index');
+    }
 }
--- pinned/app/Http/Controllers/RolController.php
+++ pinned/app/Http/Controllers/RolController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Rol;
 use Illuminate\Http\Request;
 
