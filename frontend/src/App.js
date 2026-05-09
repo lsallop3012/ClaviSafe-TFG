8a40b7222855ba72d2ce23214982a484f9dadf62
lsallop3012 <lsallop3012@g.educaand.es>
2026-05-09 22:32:48 +0200

    feat: initialize frontend with create-react-app structure
    - Add package.json with dependencies for React and testing libraries
    - Create index.html for the main HTML structure
    - Add manifest.json for PWA support
    - Include robots.txt for web crawling instructions
    - Create App.css for styling the main application
    - Implement App.js as the main React component
    - Add App.test.js for basic testing of the App component
    - Create index.css for global styles
    - Set up index.js as the entry point for the React application
    - Implement reportWebVitals.js for performance measurement
    - Add setupTests.js for configuring testing environment with jest-dom

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <p>
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default App;
