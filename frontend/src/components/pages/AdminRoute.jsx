7e436876ccf88469fd11fbc338b32500a367848c
lsallop3012 <lsallop3012@g.educaand.es>
2026-05-16 03:03:50 +0200

    Friday 15-changes made

--- frontend/src/components/pages/AdminRoute.jsx
+++ frontend/src/components/pages/AdminRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles/Admin.module.css'
-import CRUDBoards from "../components/CRUDBoards.jsx";
+import CRUDBoards from "../components/crud/CRUDBoards.jsx";
 
 export default function AdminRoute() {
   return (
