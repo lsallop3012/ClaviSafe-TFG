189622bd37084fdd86de237118eec130d21de1c7
lsallop3012 <lsallop3012@g.educaand.es>
2026-05-19 20:13:57 +0200

    update

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -7,17 +7,9 @@ use App\Enums\RoleSlug;
 
 class Role extends Model
 {
-<<<<<<< HEAD
     public $timestamps = false;
     
     protected $casts = [
-=======
-    // No requiere timestamps
-    public $timestamps = false;
-    
-    protected $casts = [
-        // Transforma automáticamente el campo 'slug' a la enumeración RoleSlug al acceder a él
->>>>>>> 346013204ac35c6a35bf1f1bb8275a080992db44
         'slug' => RoleSlug::class,
     ];
 }
